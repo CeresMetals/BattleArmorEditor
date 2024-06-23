39edb50f2540f960c0a105f23859a4b4a0ecaf64
Jeremy Misavage <jmisavage@gmail.com>
2024-06-22 20:20:01 -0400

    feat: update layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { ChassisCard } from "./components/editor/chassis";
 import { ManipulatorCard } from "./components/editor/manipulators";
 import { MovementCard } from "./components/editor/movement";
 import { ArmorCard } from "./components/editor/armor";
+import { Statusbar } from "./components/editor/statusbar";
 
 function App() {
     return (
         <DesignContextProvider>
-            <div className="flex flex-row space-x-2">
+            <div className="grid grid-cols-4 gap-2">
                 <ChassisCard />
                 <MovementCard />
                 <ManipulatorCard />
                 <ArmorCard />
+                <Statusbar />
             </div>
         </DesignContextProvider>
     );
